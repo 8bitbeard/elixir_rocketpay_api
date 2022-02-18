1a5d3ca04acfe10de6d5455f399f4645dbb7d961
Wilton Souza <willsouzafilho@gmail.com>
2022-02-18 09:26:22 -0300

    Fixing formatter warnings

--- lib/rocketpay_web/auth/error_handler.ex
+++ lib/rocketpay_web/auth/error_handler.ex
@@ -9,6 +9,6 @@ defmodule RocketpayWeb.Auth.ErrorHandler do
 
     conn
     |> Conn.put_resp_content_type("application/json")
-    |> Conn.send_resp(401,  body)
+    |> Conn.send_resp(401, body)
   end
 end
